9795d59d8a64497fcab21e79a09d82a43fab8b21
Yahia Ahmed <yahiaeltouny1980@gmail.com>
2025-04-09 22:57:22 +0200

    added user model

--- backend/src/models/user.model.mjs
+++ backend/src/models/user.model.mjs
@@ -1,8 +1,15 @@
 import {Schema, model} from "mongoose"
 
 const userSchema = new Schema({
-    username: String,
-    password: String,
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["admin", "buyer", "seller"],
+      required: true
+    },
+    email: { type: String, required: true },
+    phone: { type: String }
 })
 
 export default model("User", userSchema);
--- backend/src/validators/user.validator.mjs
+++ backend/src/validators/user.validator.mjs
@@ -1,8 +1,10 @@
 import vine from "@vinejs/vine"
 
 const userValidator = vine.object({
-    username: vine.string().minLength(5).maxLength(40).trim(),
-    password: vine.string().minLength(5).maxLength(40).trim(),
-})
-
+    username: vine.string().trim().minLength(1).required(),
+    password: vine.string().trim().minLength(1).required(),
+    role: vine.enum(["admin", "buyer", "seller"]).required(),
+    email: vine.string().email().required(),
+    phone: vine.string().optional()
+  })
 export default vine.compile(userValidator);
