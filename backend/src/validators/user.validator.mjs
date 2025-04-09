d76b97be0b7ae383deac60be96cf533d5047726c
Yahia Ahmed <yahiaeltouny1980@gmail.com>
2025-04-09 23:01:49 +0200

    lamo2a5za

--- backend/src/validators/user.validator.mjs
+++ backend/src/validators/user.validator.mjs
@@ -1,8 +1,8 @@
 import vine from "@vinejs/vine"
 
 const userValidator = vine.object({
-    username: vine.string().trim().minLength(1).required(),
-    password: vine.string().trim().minLength(1).required(),
+    username: vine.string().trim().minLength(1).maxLength(15).required(),
+    password: vine.string().trim().minLength(8).required(),
     role: vine.enum(["admin", "buyer", "seller"]).required(),
     email: vine.string().email().required(),
     phone: vine.string().optional()
