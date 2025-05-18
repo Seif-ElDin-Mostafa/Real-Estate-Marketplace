33cdfdc47838b0fc021aaf236524210a6e204587
Yahia Ahmed <yahiaeltouny1980@gmail.com>
2025-05-19 00:08:59 +0300

    changed create property so it can only accept existing Ids as the seller id

--- backend/src/controllers/property.controller.mjs
+++ backend/src/controllers/property.controller.mjs
@@ -1,10 +1,27 @@
 import Properties from "../models/property.model.mjs";
+import Users from "../models/user.model.mjs";
 
 export const createProperty = async (req, res, next) => {
   try {
     const { body } = req;
+    const { sellerId } = body;
+    const existingUser = await Users.findById(sellerId);
+    if (!existingUser) {
+      return res.status(400).send({
+        success: false,
+        message: 'Seller ID does not correspond to an existing user',
+        error: 'Invalid Seller ID'
+      });
+    }
+
+   
     const property = await Properties.create(body);
-    return res.status(201).send({ success: true, data: property, message:"Property Created", error:null});
+    return res.status(201).send({
+      success: true,
+      data: property,
+      message: "Property Created",
+      error: null
+    });
   } catch (error) {
     next(error);
   }
